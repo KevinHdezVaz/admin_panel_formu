353962bce853f827c1b3c4ddfec1b9469e3c8928
kevin hernandez <kevin.hernandez@caffenio.com>
2023-06-06 22:36:33 -0600

    first commit

---
+++ config/constant.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'apikey' => [
+        'server_key' => 'AAAAP96HKEY:APA91bGyTwMouB9K9OuYZguwZsgNwctVvtI1mOR8NDEO7-dzObqzk5TEXhKG5t6SbVb6hp5EKiSxRlmC0rtiPY1m-SpXrFL_XL2WMC5QU9iFgE_6hrM34s39fZTc8FDtazC0CfR1Wj9d'
+      ],
+];
